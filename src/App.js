236a1cc3fa72c60aa6e60c394d04b7c0ba36ac70
mnava1418 <mnavapena@gmail.com>
2021-02-27 15:12:59 -0600

    set bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { main, selectName } from './features/rootReducer'
+//import { useSelector, useDispatch } from 'react-redux';
+//import { main, selectName } from './features/rootReducer'
 
 function App() {
-  const name = useSelector(selectName)
+  /*const name = useSelector(selectName)
   const dispatch = useDispatch();
-  dispatch(main())
-
+  dispatch(main())*/
   return (
     <div>
-        {name}
     </div>
   );
 }
