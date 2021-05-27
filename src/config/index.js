836739662bb3b53e2a8b1c56e01be4805ef17e7d
mnava1418 <mnavapena@gmail.com>
2021-05-26 19:13:46 -0500

    refactor app

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export const app = {
+    email: 'mnavapena@gmail.com',
+    phone: '+52 55 2972 8441'
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/js/dist/collapse'
-import './index.css';
+import '../src/styles/fonts.css';
+import '../src/styles/App.css';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
