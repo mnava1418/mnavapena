e518713d9be4cced5dd13f97b56b00f04d8adaf7
mnava1418 <mnavapena@gmail.com>
2021-05-27 20:15:15 -0500

    add icons

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,7 @@
 export const app = {
     email: 'mnavapena@gmail.com',
-    phone: '+52 55 2972 8441'
+    phone: '+52 55 2972 8441',
+    linkedin: 'https://www.linkedin.com/in/martin-nava-pe%C3%B1a-4b7219a0',
+    github: 'https://github.com/mnava1418',
+    whatsapp: 'https://api.whatsapp.com/send?text=¡Hi%20Martin!&phone=525529728441'
 }
