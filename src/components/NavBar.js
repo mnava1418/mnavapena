9ff1d70ff91fa6301b4b8317994b9b7a736fdd8d
mnava1418 <mnavapena@gmail.com>
2021-07-02 18:44:44 -0500

    UPDATE: create dynamic menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,31 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import {app} from '../config'
+import '../styles/NavBar.css'
+
+const getMenuItems = (menu) => {
+    if(menu) {
+        return (
+            <Nav className="mr-auto">
+                {Object.keys(menu).map((key) => (<Nav.Link key={key} href={`#${key}`}>{menu[key]}</Nav.Link>))}
+            </Nav>
+        )
+    }
+}
 
 class MainBar extends React.Component {
     render() {
+        const {menu} = this.props
         return (
             <Navbar fixed="top" variant="dark" expand="lg" className="main-navBar">
                 <Navbar.Brand href={`mailto:${app.email}`} className="navBar-brand">{app.email}</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#services">Services</Nav.Link>
-                        <Nav.Link href="#resume">Resume</Nav.Link>
-                    </Nav>
+                    {getMenuItems(menu)}
                 </Navbar.Collapse>
             </Navbar>
         )
     }
 }
 
-export default MainBar;
+export default MainBar
