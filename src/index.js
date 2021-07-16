7bae115d9070cd65c800ddd429ffb1c4f7979037
mnava1418 <mnavapena@gmail.com>
2021-07-15 19:10:15 -0500

    UPDATE: Stop using redux and change styles

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../src/styles/fonts.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import configureStore from './store/configureStore'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={configureStore()}>
-      <App />
-    </Provider>
+  <React.StrictMode>    
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
